bc9fba6a17b2630e2c08c19124eea445b508aa8b
PhantomClick <citak.erol@gmail.com>
2019-06-18 11:27:35 +0300

    Update recognition_app.py

--- recognition/recognition_app.py
+++ recognition/recognition_app.py
@@ -1,132 +1,209 @@
-from flask import Flask, redirect, url_for, request, render_template, jsonify, session
+from flask import Flask,flash,redirect, url_for, request, render_template, jsonify, session
 from werkzeug import security
 from werkzeug.utils import secure_filename
 import os
 import requests
 import cv2
 import numpy as np
-import pytesseract
+import PIL.Image
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-BINARY_THRESHOLD = 180
 
-def image_smoothing(img):
-	ret1, th1 = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
-	ret2, th2 = cv2.threshold(th1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-	blur = cv2.GaussianBlur(th2, (1,1), 0)
-	ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+@app.route("/upload")
+def upload_image():
+   print("Upload Controller Working")
+   return render_template('upload.html')
 
-	return th3
 
-def get_hsv(img, channel):
+@app.route('/image_app', methods = ['GET', 'POST'])
+def upload_img():
 
-	return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:,:,channel]
+   print("Image app")
+   UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
 
-@app.route('/txt_recognize', methods=['GET', 'POST'])
-def txt_recognize():
-	im_bgr = request.files["bgr_image"]
-	im_mask = request.files["mask_image"]
+   if request.method == 'POST':
+      # check if the post request has the file part
+      if 'file' not in request.files:
+         print('No file part')
+         return jsonify({'result' : 'No file part!..'})
+      file = request.files['file']
+      # if user does not select file, browser also
+      # submit a empty part without filename
+      if file.filename == '':
+         print('No selected file')
+         return jsonify({'result' : 'No selected file!..'})
+
+      if file:
+         filename = secure_filename(file.filename)
+         file.save(os.path.join(UPLOAD_FOLDER,"original_image.jpg"))
+         #file.save(os.path.join(UPLOAD_FOLDER, "img.jpg"))
+
+   return jsonify({"result":"Saving O.k.", "filename": file.filename})
+   #return jsonify({"result":"Saving O.k.", "filename": "img.jpg"})
+
+@app.route('/image_delete', methods = ['GET', 'POST'])
+def delete_img():
+
+   print("Image delete")
+   UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
+
+   if request.method == 'POST':
+      # check if the post request has the file part
+      if not(os.path.isdir(UPLOAD_FOLDER)):
+         print('No such a directory')
+         return jsonify({'result' : 'No such a directory!..'})
+      files = os.listdir(UPLOAD_FOLDER)
+      print("There are # files:", len(files))
+
+      for the_file in os.listdir(UPLOAD_FOLDER):
+         file_path = os.path.join(UPLOAD_FOLDER, the_file)
+         try:
+            if os.path.isfile(file_path):
+               os.unlink(file_path)
+            # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+         except Exception as e:
+            print(e)
+
+   return jsonify({"result":"Deletion O.k."})
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
+############################################################################################################
+
+@app.route('/text_detect', methods=['GET', 'POST'])
+def index(): 
+	st_code = 0
+
 	UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
-
-	im_bgr.save(os.path.join(UPLOAD_FOLDER, 'tmp.jpg'))
-	bgr_im = cv2.imread(os.path.join(UPLOAD_FOLDER, 'tmp.jpg'))
-
-	im_mask.save(os.path.join(UPLOAD_FOLDER, 'tmp_mask.jpg'))
-	mask_im = cv2.imread(os.path.join(UPLOAD_FOLDER, 'tmp_mask.jpg'))
-
-	print(mask_im.shape)
-	print(np.max(mask_im))
-
-	bgr_im = cv2.resize(bgr_im, (1280,1280))
-
-	# CCA 
-	mask_im = cv2.cvtColor(mask_im, cv2.COLOR_BGR2GRAY)
-	ret, comp = cv2.connectedComponents(mask_im, 16)
-
-	mask = np.array(comp, dtype=np.uint8)
-	for label in range(1, ret):
-		mask[comp == label] = 255
-
-	kernel_sqr = np.ones((5,5), np.uint8)
-	mask = cv2.dilate(mask, kernel_sqr, iterations=2)
-	mask = cv2.erode(mask, kernel_sqr, iterations=2)
-
-
-	output = open(os.path.join(UPLOAD_FOLDER,"output_text.txt"),"w",encoding="utf-8")
-	config = ('-l eng --oem 1 --psm 3')
-
-	# find contours
-	contours = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-
-	padding = 10
-	counter = 0
-	for cnt in contours:
-		(x,y,w,h) = cv2.boundingRect(cnt)
-
-		a = y - padding
-		b = x - padding
-		c = y + h + padding
-		d = x + w + padding
-
-		# If padding exceeds from our borders
-		if y - padding < 0:
-			a = y
-		if x - padding < 0:
-			b = x
-		if y + h + padding > x:
-			c = y + h
-		if x + w + padding > y:
-			d = x + w
-
-		ROI = bgr_im[a:c, b:d]
-
-		ROI = image_smoothing(get_hsv(ROI,2))
-
-		
-		x,y = ROI.shape
-		col_val = ROI[0,0]
-
-		if col_val < 128:
-			x,y = ROI.shape
-
-			for i in range(x):
-				for j in range(y):
-					ROI[i,j] = np.abs(255 - ROI[i,j])
-		
-		kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-		ROI = cv2.filter2D(ROI, -1, kernel)
-		
-		output.write("- "+ pytesseract.image_to_string(ROI, config=config)+ "\n")
-		cv2.imwrite(os.path.join(UPLOAD_FOLDER, 'Sharp_Image'+str(counter)+'.jpg'), ROI)
-		counter += 1
-
-	bitwised_image = cv2.bitwise_and(get_hsv(bgr_im,2),mask_im,mask_im)
-	cv2.imwrite(os.path.join(UPLOAD_FOLDER, 'Bitwised_Image.jpg'), bitwised_image)
-	output.close()
+	filename = os.listdir(UPLOAD_FOLDER)[0]
+	print(filename)
+	path_img = os.path.join(UPLOAD_FOLDER, filename)
 	
-	
-	return 'Sharpening Success!..'
+	with open(path_img, "rb") as image_file:
+		files = {'field_name': image_file}
 
-@app.route('/get_txt_recognize', methods=['GET', 'POST'])
-def get_txt_recognize():
+		r = requests.post('http://tds/txt_detection', files=files )
+		#print(r.status_code)
+		txt = r.text
+		print(txt)
 	
+	return jsonify({"result":"Text Detection O.k."})
+
+
+@app.route('/get_text_detect', methods=['GET', 'POST'])
+def get_index(): 
 	UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
-	filename = "Sharp_Image.jpg"
+	
+	r = requests.get('http://tds/get_txt_detection')
+
+	#nparr = np.fromstring(r.data, np.uint8)
+	#im_gray = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+	#print(r.content.tofile().shape)
+	#decoded = cv2.imdecode(np.frombuffer(r.content, np.uint8), -1)
+	decoded = np.fromstring(r.content, np.uint8)
+	decoded = cv2.imdecode(decoded, 0)
+	
+	cv2.imwrite(os.path.join(UPLOAD_FOLDER, 'Binary_Image.jpg'), decoded)
+
+	
+	return jsonify({"result":"Getting Text Detection O.k."})
+
+############################################################################################################
+
+@app.route('/text_preprocess', methods=['GET', 'POST'])
+def index_preprocess(): 
+	st_code = 0
+
+	UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
+	filename = os.listdir(UPLOAD_FOLDER)[0]
+	print(filename)
 	path_img = os.path.join(UPLOAD_FOLDER, filename)
 
-	sharp_im = cv2.imread(os.path.join(UPLOAD_FOLDER, 'Sharp_Image.jpg'),0)
-	print(sharp_im.shape)
+	
+	with open(path_img, "rb") as image_file:
+		files = {'field_name': image_file}
 
-	# encode image as jpeg
-	_, img_encoded = cv2.imencode('.jpg', sharp_im)
+		r = requests.post('http://tps/txt_preprocess', files=files )
+		#print(r.status_code)
+		txt = r.text
+		print(txt)
+	
+	return jsonify({"result":"Preprocessing O.k.", "path":os.path.join(UPLOAD_FOLDER, "Binary_Image.jpg")})
 
-	#return send_file(os.path.join(UPLOAD_FOLDER, 'Gray_Image.jpg'), mimetype='image/jpg', attachment_filename='Gray_Image.jpg')
-	#ilename = os.path.join(UPLOAD_FOLDER, 'Gray_Image.jpg')
-	#return send_file(filename, mimetype='image/jpg')
 
-	return img_encoded.tostring()
+@app.route('/get_text_preprocess', methods=['GET', 'POST'])
+def get_index_preprocess(): 
+	UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
+	
+	r = requests.get('http://tps/get_txt_preprocess')
+
+	#nparr = np.fromstring(r.data, np.uint8)
+	#im_gray = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+	#print(r.content.tofile().shape)
+	#decoded = cv2.imdecode(np.frombuffer(r.content, np.uint8), -1)
+	decoded = np.fromstring(r.content, np.uint8)
+	decoded = cv2.imdecode(decoded, 0)
+	
+	cv2.imwrite(os.path.join(UPLOAD_FOLDER, 'Final_Binary_Image.jpg'), decoded)
+
+	
+	return jsonify({"result":"Getting Preprocessing O.k."})
+############################################################################################################
+
+@app.route('/text_recognize', methods=['GET', 'POST'])
+def index_recognize(): 
+	st_code = 0
+
+	UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
+	filename_bgr = "original_image.jpg"
+	filename_mask = "Final_Binary_Image.jpg"
+
+	path_bgr = os.path.join(UPLOAD_FOLDER, filename_bgr)
+	path_mask = os.path.join(UPLOAD_FOLDER, filename_mask)
+
+	file_list = [  
+       ('bgr_image', ('original_image.jpg', open(path_bgr, 'rb'), 'image/png')),
+       ('mask_image', ('Final_Binary_Image.jpg', open(path_mask, 'rb'), 'image/png'))
+   ]
+
+	with open(path_bgr, "rb") as image_file:
+		files_bgr = {'bgr_image': image_file}
+	with open(path_mask, "rb") as image_file:
+		files_mask = {'mask_image': image_file}
+
+	r = requests.post('http://trs/txt_recognize', files=file_list )
+	#print(r.status_code)
+	txt = r.text
+	print(txt)
+
+	
+	return jsonify({"result":"Sharpening O.k.", "path":os.path.join(UPLOAD_FOLDER, "Sharp_Image.jpg")})
+
+
+@app.route('/get_text_recognize', methods=['GET', 'POST'])
+def get_index_recognize(): 
+	UPLOAD_FOLDER = os.path.join(os.path.join(APP_ROOT,'static'),'image')
+	
+	r = requests.get('http://trs/get_txt_recognize')
+
+	#nparr = np.fromstring(r.data, np.uint8)
+	#im_gray = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+	#print(r.content.tofile().shape)
+	#decoded = cv2.imdecode(np.frombuffer(r.content, np.uint8), -1)
+	decoded = np.fromstring(r.content, np.uint8)
+	decoded = cv2.imdecode(decoded, 1)
+	
+	cv2.imwrite(os.path.join(UPLOAD_FOLDER, 'Sharp_Image.jpg'), decoded)
+
+	
+	return jsonify({"result":"Getting Sharpening Image O.k."})
+
+
+############################################################################################################
+
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=80, debug=True)
